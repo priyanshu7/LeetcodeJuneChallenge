cd103e1595be73af549410d0c818fe9514b0808f
Priyanshu Mehta <mehta7priyanshu@gmail.com>
2020-06-25 15:34:47 +0530

    day 25

---
+++ Day 25/FindDuplicateNumber.java
@@ -0,0 +1,20 @@
+package june2020;
+
+public class FindDuplicateNumber {
+	
+	public int findDuplicate(int[] nums) {
+        int tortoise = nums[0], hare = nums[0];
+        do{
+            tortoise = nums[tortoise];
+            hare = nums[nums[hare]];
+        }while(tortoise != hare);
+        
+        tortoise = nums[0];
+        while(tortoise != hare){
+            tortoise = nums[tortoise];
+            hare = nums[hare];
+        }
+        return tortoise;
+    }
+
+}
